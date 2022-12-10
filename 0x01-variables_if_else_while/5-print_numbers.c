fad2344b0ad5924430bd43e0fbc1c1612232fc14
Calvin <johncalvinkarume@gmail.com>
2022-12-09 17:36:41 -0800

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,31 +1,15 @@
-#include <stdio.h>                                                                                                                      
-
-/**                                                                                                                                     
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
  *
- *  * main - prints all single digit numbers of base 10 starting from 0                                                                    
- *
- *   *                                                                                                                                      
- *
- *    * Return: Always 0 (Success)                                                                                                           
- *
- *     */                                                                                                                                     
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
 
-                                                                                                                                        
-
-int main(void)                                                                                                                          
-
-{                                                                                                                                       
-
-	        int a;                                                                                                                          
-
-		                                                                                                                                        
-
-		        for (a = 0; a < 10; a++)                                                                                                        
-
-				                printf("%d", a);                                                                                                        
-
-			        putchar('\n');                                                                                                                  
-
-				        return (0);                                                                                                                     
-
-}       
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
