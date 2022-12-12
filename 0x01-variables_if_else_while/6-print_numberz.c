3afa2e731e6df4f567e49a7cd62e4b8661232da4
Calvin <johncalvinkarume@gmail.com>
2022-12-12 03:51:29 -0800

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,16 +2,18 @@
 /**
  * main - prints all single digit numbers of base 10 starting from 0
  *
- * Return: Always 0 (Success
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int c = 0;
-	while (c < 10);
-	{
-		putchar(c =+ '0');
-		c++;
-	}
+	int c;
+
+	c = 0;
+	while
+		(c < 10) {
+			putchar(c + '0');
+			c++;
+		}
 	putchar('\n');
 	return (0);
 }
