45830646a5cb82f37296d39f1727cdb1638f57d0
Calvin <johncalvinkarume@gmail.com>
2022-12-12 03:42:33 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0 (Success
+ */
+int main(void)
+{
+	int c = 0;
+	while (c < 10);
+	{
+		putchar(c =+ '0');
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
