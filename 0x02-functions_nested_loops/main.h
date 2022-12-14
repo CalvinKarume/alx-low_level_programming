b9bd7c1a05776afd600c73863e8ee5fc37588585
Calvin <johncalvinkarume@gmail.com>
2022-12-14 14:39:24 -0800

    Task 0

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,25 @@
+int _putchar(char c);
+
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _abs(int);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_last_digit(int);
+
+int print_sign(int n);
+
+int add(int, int);
+
+void jack_bauer(void);
+
+void print_to_98(int n);
+
+void times_table(void);
+
+void print_times_table(int n);
