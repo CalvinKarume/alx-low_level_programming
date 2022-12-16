6a2522f8145aee697b80fa578fd6cf9f4777cf01
Calvin <johncalvinkarume@gmail.com>
2022-12-16 01:53:12 -0800

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mainn - tests if integer is positive or negative
+ * Return: Always zero
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
