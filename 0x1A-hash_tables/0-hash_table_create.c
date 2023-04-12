188385a4b00504eef400d8b6ee559fd7b36823e9
Calvin <johncalvinkarume@gmail.com>
2023-04-12 05:21:49 -0700

    Complete Task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,18 +8,29 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
-	unsigned long int i;
+	hash_node_t **array;
+	unsigned long int i = 0;
+
+	if (size == 0)
+		return (NULL);
 
 	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
+	if (!ht)
 		return (NULL);
+	array = malloc(sizeof(*array) * size);
+	if (!array)
+	{
+		free(ht);
+		return (NULL);
+	}
+	while (i < size)
+	{
+		array[i] = NULL;
+		i++;
+	}
 
 	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
-		return (NULL);
-	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
+	ht->array = array;
 
 	return (ht);
 }
